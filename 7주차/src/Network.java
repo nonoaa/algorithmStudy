20c157a99cf9f82e91702ec372f4652e540700e3
nonoaa <nonoaa1@naver.com>
2020-08-24 03:14:33 +0900

    d

--- 7주차/src/Network.java
+++
@@ -1,9 +0,0 @@
-public class Network {
-    public static void main(String[] args) {
-
-    }
-    public int solution(int n, int[][] computers) {
-        int answer = 0;
-        return answer;
-    }
-}
