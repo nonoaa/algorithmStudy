1a1bac29f8c11b1d7c5643c70d8d8a04078e62d8
nonoaa <nonoaa1@naver.com>
2021-09-24 21:03:20 +0900

    Level2

---
+++ 개인공부/programmers/sort/h_index.cpp
@@ -0,0 +1,20 @@
+//https://programmers.co.kr/learn/courses/30/lessons/42747
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    int answer = 0;
+    
+    sort(citations.begin(), citations.end(), greater<int>());
+    for (int i = 0; i < citations.size(); i++)
+    {
+        if (citations[i] >= i + 1)
+            answer = i + 1;
+    }
+    
+    return answer;
+}
